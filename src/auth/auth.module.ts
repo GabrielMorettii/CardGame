19d82eb48debfc412f502275ff528e5be35bc09b
GabrielMorettii <gabrielmorettisilva@gmail.com>
2022-10-29 09:32:17 -0300

    GetUser Decorator and Integration with module Users

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
+import { JwtGuard } from './guards/jwt.guard';
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, JwtService],
+  providers: [AuthService, JwtService, JwtStrategy, JwtGuard],
 })
 export class AuthModule {}
---
+++ src/auth/decorators/get-user.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    if (data) {
+      return request.user[data as string];
+    }
+
+    return request.user;
+  },
+);
