0657942e111bfd776f8ada9b4b680a07294fd3e5
GabrielMorettii <gabrielmorettisilva@gmail.com>
2022-10-29 09:08:37 -0300

    Jwt Strategy and Guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos';
+import { AuthUserDto } from './dtos/AuthUserDto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  private signup(@Body() data: CreateUserDto) {
-    return data;
+  private async signup(@Body() data: CreateUserDto) {
+    return await this.authService.signup(data);
   }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  private async signin(@Body() data: AuthUserDto) {
+    return await this.authService.signin(data);
+  }
+
+  // @Get('google')
+  // private googleAuth() {}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { CreateUserDto } from './dtos';
+import * as argon2 from 'argon2';
+import { AuthUserDto } from './dtos/AuthUserDto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private config: ConfigService,
+    private jwt: JwtService,
+  ) {}
+
+  public async signup(data: CreateUserDto) {
+    const emailAlreadyExists = await this.prismaService.user.findFirst({
+      where: { email: data.email },
+    });
+
+    if (emailAlreadyExists)
+      throw new BadRequestException('Email already exists!');
+
+    data.password = await argon2.hash(data.password);
+
+    const user = await this.prismaService.user.create({ data });
+
+    delete user.password;
+
+    return user;
+  }
+
+  public async signin(data: AuthUserDto) {
+    const userExists = await this.prismaService.user.findFirst({
+      where: { email: data.email },
+    });
+
+    if (!userExists) throw new UnauthorizedException('Invalid credentials!');
+
+    const passwordsMatches = await argon2.verify(
+      userExists.password,
+      data.password,
+    );
+
+    if (!passwordsMatches)
+      throw new UnauthorizedException('Invalid credentials!');
+
+    return await this.signToken(userExists.id);
+  }
+
+  async signToken(userId: string): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
